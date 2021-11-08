7d431cf613a046c968e83c770edc494ae61fc461
Jitendra Singh <jitendra.solanki@greatlearning.in>
2021-11-08 09:32:46 +0530

    update env file

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    emails {Faker::Internet:emails }
+    first_name {Faker::Internet:first_name }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
+
+  describe 'schema' do
+    it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:encrypted_password).of_type(:string) }
+  end
+
   describe 'validation' do
     it { User.new }
   end
